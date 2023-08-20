2b104f6242742ec828906a899458180cc5c8f79f
JoelD-418 <joeldisidi8@gmail.com>
2023-08-20 16:55:31 +0100

    nouvelle-version-affiche-les-donneés-dans-le-second-ecran

--- script.js
+++ script.js
@@ -2,26 +2,42 @@
 //cibler touts les bouttons
 //cibler l'ecran
 const screen = document.getElementById("input");
-const bouttons = document.querySelectorAll("button");
+// je cible tout les elements des type bouttons, avec leurs classe et id 
+const bouttons = document.querySelectorAll(".numpad,#divideby,#times,#minus,#plus,#equals");
+//je cible l'ecran qui va effectuer les calcules
+const screen2 = document.getElementById("calcul");
+
 for(let i = 0; i<bouttons.length; i++){
 //stocke tous les elements bouttons dans une  variable allbouttons
 const allbouttons = bouttons[i];
-//creer une variable qui stocke le type  et une condition qui doit gerer ces type de bouttons
+// console.log(allbouttons);
 allbouttons.onclick = function(event){
-  // if(document.querySelector("#plus")){
-  //  const allsub = event.preventDefault();
-  // }
-// else 
+   
 if(screen.value == "0"){
   screen.value = "";
   screen.value += event.target.innerHTML;
 }
-
 else{
   screen.value += event.target.innerHTML;
 }
-// if(screen.value ==)
+// si on clique sur = -+/* que le boutton  renvoie les chiffres au second ecran
+if(screen.value == "="){
+  event.preventDefault();
+  screen2.innerHTML += screen.value;
+}
+else if(screen.value == "+"){
+  event.preventDefault();
+  screen.innerHTML +=screen.value; 
+}
+}
+}
+
+// if(allbouttons.onclick = function(event1){
+// if(screen.value == "="){
+//   event1.preventDefault();
+//   screen2.innerHTML = "12";
+// }
+// }){
+ 
 
 // }
-}
-}

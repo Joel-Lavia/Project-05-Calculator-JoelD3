76d92d3ba207af02407f510b7123dbafe969c8c1
Hol418 <joeldisidi8@gmail.com>
2023-08-25 21:28:59 +0100

    Version-avec + et - qui-fonctionne-toujours

--- script.js
+++ script.js
@@ -10,36 +10,47 @@ const screen2 = document.getElementById("calcul");
 for(let i = 0; i<bouttons.length; i++){
 //stocke tous les elements bouttons dans une  variable allbouttons
 const allbouttons = bouttons[i];
+let div;
+let mutiplication;
+
 // console.log(allbouttons);
 allbouttons.onclick = function(event){
-   
-if(screen.value == "0"){
-  event.preventDefault();
-  screen.value = "";
-  screen.value += event.target.innerHTML;
+  //remplacer x par * et ÷ par /
+  if(screen.value == "0"){
+      event.preventDefault();
+      screen.value = "";
+      screen.value += event.target.innerHTML;
+    }
+  //le boutton AC doit reinitialiser les 2 ecrans
+  // else if(event.target.innerHTML == "AC"){
+  //   event.preventDefault();
+  //   screen2.textContent= "";
+  //   screen.value = "0";
+  // }
+  //quand on clique sur = que x soit remplacer par * et ÷ par /
+  else if(event.target.innerHTML =="="){
+    event.preventDefault();
+    // if(event.target.innerHTML == "×"){
+    //   op = event.replace("×","*");
+    //   console.log(op);
+    // }
+    // screen2.innerHTML += eval(screen.value);
+    // console.log(screen.value);
+    // if(screen.value.includes('×') ){
+    //   screen.value.replace("×","*");
+    // }
+    // else if(screen.value.includes("÷"))
+    // {
+    //   screen.value.replace("÷","/");
+    // }
+    // console.log({evevtValue: event.target.innerHTML, screenValue: screen.value});
+    screen2.innerHTML += eval(screen.value);
+    
+  }
+  else{
+      event.preventDefault();
+      screen.value += event.target.innerHTML;
+    }
 }
-//quand on clique l dois avoir les oprtations de + et -
-// cree une fonction ou quand on clique il dois avoir de calcule * et /
-else if(event.target.innerHTML == "="){
-  event.preventDefault();
-  screen2.innerHTML += eval(screen.value); 
 }
-else{
-  event.preventDefault();
-  screen.value += event.target.innerHTML;
-}
-// si on clique sur = -+/* que le boutton  renvoie les chiffres au second ecran.
-// if(screen.value == "="){
-//   screen2.textContent = eval(screen.value);
-  
-// }
-// else if(screen.value == "+"){
-//   event.preventDefault();
-//   screen2.innerHTML +=screen.value; 
-// }
-}
-}
-
- 
-
 

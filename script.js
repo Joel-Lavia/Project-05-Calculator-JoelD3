cea33e539f68afbc3d887218c263eed30203b1c9
JoelD-418 <joeldisidi8@gmail.com>
2023-08-30 02:33:31 +0100

    All operations work but not yet finalized

--- script.js
+++ script.js
@@ -6,20 +6,35 @@ const screen = document.getElementById("input");
 const bouttons = document.querySelectorAll(".numpad,#divideby,#times,#minus,#plus,#equals");
 //je cible l'ecran qui va effectuer les calcules
 const screen2 = document.getElementById("calcul");
+let screenValue = "";
 
 for(let i = 0; i<bouttons.length; i++){
 //stocke tous les elements bouttons dans une  variable allbouttons
 const allbouttons = bouttons[i];
-// console.log(allbouttons);
+//si on clique sur x qu'elle soit remplacer par *
 allbouttons.onclick = function(event){
-   
 if(screen.value == "0"){
   event.preventDefault();
   screen.value = "";
   screen.value += event.target.innerHTML;
 }
-//quand on clique l dois avoir les oprtations de + et -
-// cree une fonction ou quand on clique il dois avoir de calcule * et /
+else if(event.target.innerHTML == "×"){
+  event.preventDefault();
+  event.target.innerHTML = "*";
+}
+else if(event.target.innerHTML == "÷"){
+  event.preventDefault();
+  event.target.innerHTML = "/";
+}
+else if(event.target.innerHTML == "C"){
+  screen.value =screen.slice(0, -1);
+}
+else if (event.target.innerHTML == "AC") {
+  event.preventDefault();
+  screenValue = "";
+  screen.value += defaultValue;
+  screen2.value += screenValue;
+}
 else if(event.target.innerHTML == "="){
   event.preventDefault();
   screen2.innerHTML += eval(screen.value); 
@@ -28,18 +43,7 @@ else{
   event.preventDefault();
   screen.value += event.target.innerHTML;
 }
-// si on clique sur = -+/* que le boutton  renvoie les chiffres au second ecran.
-// if(screen.value == "="){
-//   screen2.textContent = eval(screen.value);
-  
-// }
-// else if(screen.value == "+"){
-//   event.preventDefault();
-//   screen2.innerHTML +=screen.value; 
-// }
 }
 }
-
- 
 
 

7a24a9272a8063be10cf622b3d3227375dfa3d6c
Hol418 <joeldisidi8@gmail.com>
2023-10-09 20:15:07 +0100

    this event blocks the physical keys of the keyboard

--- script.js
+++ script.js
@@ -1,53 +1,18 @@
 // TODO: Faire la manipulation du DOM dans ce fichier
 //cibler touts les bouttons
 //cibler l'ecran
-const screen = document.getElementById("input");
-// je cible tout les elements des type bouttons, avec leurs classe et id 
+//1.Bloquer les bouttons physique
+//2.rendre les bouttons cliquable
+const screeninput = document.getElementById("input");
+//je cible tout les elements des type bouttons, avec leurs classe et id 
 const bouttons = document.querySelectorAll("button");
 //je cible l'ecran qui va effectuer les calcules
-<<<<<<< HEAD
-const screen2 = document.getElementById("calcul");
-=======
-const screen2 = document.getElementById("calcul");
-let screenValue = "";
+const screenresult = document.getElementById("calcul");
+for(let i = 0; i<bouttons.length ;i++){
+const boutton = bouttons[i];
+window.addEventListener("keydown",function(event){event.preventDefault()});
 
-for(let i = 0; i<bouttons.length; i++){
-//stocke tous les elements bouttons dans une  variable allbouttons
-const allbouttons = bouttons[i];
-//si on clique sur x qu'elle soit remplacer par *
-allbouttons.onclick = function(event){
-if(screen.value == "0"){
-  event.preventDefault();
-  screen.value = "";
-  screen.value += event.target.innerHTML;
-}
-else if(event.target.innerHTML == "×"){
-  event.preventDefault();
-  event.target.innerHTML = "*";
-}
-else if(event.target.innerHTML == "÷"){
-  event.preventDefault();
-  event.target.innerHTML = "/";
-}
-else if(event.target.innerHTML == "C"){
-  screen.value =screen.slice(0, -1);
-}
-else if (event.target.innerHTML == "AC") {
-  event.preventDefault();
-  screenValue = "";
-  screen.value += defaultValue;
-  screen2.value += screenValue;
-}
-else if(event.target.innerHTML == "="){
-  event.preventDefault();
-  screen2.innerHTML += eval(screen.value); 
-}
-else{
-  event.preventDefault();
-  screen.value += event.target.innerHTML;
-}
-}
-}
+  
+};
 
 
->>>>>>> cea33e539f68afbc3d887218c263eed30203b1c9

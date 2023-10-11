f9b6329685b26950b388cedf9d3b2fe91877a024
Hol418 <joeldisidi8@gmail.com>
2023-10-11 20:41:08 +0100

    multiplication calculation update

--- script.js
+++ script.js
@@ -7,31 +7,68 @@
 
 /*3.Faire en sorte que quand il y'a un event onclick sur un operateur les données entrées
  dans le screeninput monte sur le screenresult*/
+ //4.remplacer x par * et ÷ par / a partir de la methode replace 
 
-const screeninput = document.getElementById("input");
+const screenInput = document.getElementById("input");
 //je cible tout les elements des type bouttons, avec leurs classe et id 
 const bouttons = document.querySelectorAll("button");
 //je cible l'ecran qui va effectuer les calcules
-const screenresult = document.getElementById("calcul");
+const screenResult = document.getElementById("calcul");
+
+// const equalBtn = document.getElementById("equals");
+// equalBtn.addEventListener("click",function(event){
+//     event.preventDefault();
+//     let result = screeninput.value.replace(timesBtn.innerText,"*");
+
+//     result = eval(result);
+//     screeninput.value = result;
+                   
+// });
+
+// const timesBtn = document.getElementById("times");
+// timesBtn.addEventListener("click",function(event){
+// event.preventDefault();
+// screeninput.value += timesBtn.innerText;
+// });
+
 for(let i = 0; i<bouttons.length ;i++){
-// const boutton = bouttons[i];
-//cette event bloque les bouttons physique du clavier
-window.addEventListener("keydown",function(event){event.preventDefault()});
-//cette event rend le boutton numérique cliquable et fait des operations 
-document.addEventListener("click",function(event){
+const boutton = bouttons[i];
+// cette event bloque les bouttons physique du clavier
+// window.addEventListener("keydown",function(event){event.preventDefault()});
+// cette event rend le boutton numérique cliquable et fait des operations 
+boutton.addEventListener("click",function(event){
 event.preventDefault();
 //rends les boutton numerique cliquable
-if(screeninput.value == "0"){
-screeninput.value ="";
-screeninput.value +=event.target.innerText;
+let resultat;
+if(screenInput.value == "0"){
+    screenInput.value ="";
+    screenInput.value += event.target.innerText;
 }
 /*3.Faire en sorte que quand il y'a un event onclick sur un operateur les données entrées
  dans le screeninput monte sur le screenresult*/
-else if(event.target.innerText == "+"){
-screenresult.innerHTML = (screeninput.value) ;
+//  else if(event.target.innerText == "×"){
+//     let resultat = screenInput.value.replace("×","*");
+//  }
+//  else if(screenInput.value == "÷"){
+//     let division = screenInput.value;
+//     division.replace(screenInput.value,"/");
+//     // console.log(division.replace(screenInput.value,"/"));
+//  }
 
+//  else if(event.target.innerText == "="){ 
+//      resultat = screenInput.value.replace("÷", "/");
+//      screenResult.innerText +=eval(resultat); 
+//  }
 
- }
+ else if(event.target.innerText == "="){ 
+    resultat = screenInput.value.replace("×", "*");
+    screenResult.innerText +=eval(resultat); 
+}
+ 
+ else {
+screenInput.value += event.target.innerText;
+  }
+
 
 
 
